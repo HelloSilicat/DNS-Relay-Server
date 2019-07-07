0015b7f43f8c04426780e4b36bc87cbbf2bc12e0
jialiangpei <453715647pjl@gmail.com>
2019-07-07 18:09:46 +0800

    add configure

--- start.py
+++ start.py
@@ -1,6 +1,24 @@
 from Server import DNSRelayServer
-
+import getopt
+import sys
 
 if __name__ == '__main__':
-    server = DNSRelayServer()
+    opts, args = getopt.getopt(sys.argv[1::], 'f:r:t:b:')
+
+    remote_dns = '10.3.9.5' 
+    local_dns_file = "./dnsrelay.txt"
+    dns_buffer_file = "./dnsbuffer.txt"
+    flush = False
+
+    for op, value in opts:
+        if op == '-f':
+            flush = True if value == "=True" or value == "True" else False
+        elif op == '-r':
+            remote_dns = value
+        elif op == '-t':
+            local_dns_file = value
+        elif op == '-b':
+            dns_buffer_file=value
+
+    server = DNSRelayServer(remote_dns=remote_dns, local_dns_file=local_dns_file, dns_buffer_file=dns_buffer_file, flush=flush)
     server.start()
